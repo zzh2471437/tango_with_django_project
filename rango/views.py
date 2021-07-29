359859b48eb3c5b71d58ebc8c19a74cff1e3ae51
“Zhehao <2471437Z@student.gla.ac.uk>
2021-07-29 21:28:35 +0100

    Chapter4

--- rango/views.py
+++ rango/views.py
@@ -4,5 +4,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
+    context_dict={'boldmessage':'Crunchy, creamy, cookie, candy, cupcake!'}
+    returnrender(request,'rango/index.html', context=context_dict)
 def about(request): 
     return HttpResponse("Rango says here is the about page <a href='/rango/'>index</a>") 
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -14,6 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 
 # Quick-start development settings - unsuitable for production
@@ -63,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -119,3 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [STATIC_DIR,]
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
+
+
+#print(__file__)
+#print(os.path.dirname(__file__))
+#print(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
