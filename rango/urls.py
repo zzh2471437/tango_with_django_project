7331be9988363ee63ebb3157d743e1c66d3f52da
“Zhehao <2471437Z@student.gla.ac.uk>
2021-07-29 23:13:03 +0100

    Chapter4 exercise

--- rango/urls.py
+++ rango/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'rango'
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
-    context_dict={'boldmessage':'Crunchy, creamy, cookie, candy, cupcake!'}
-    returnrender(request,'rango/index.html', context=context_dict)
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
 def about(request): 
     return HttpResponse("Rango says here is the about page <a href='/rango/'>index</a>") 
