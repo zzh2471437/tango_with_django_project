8b127411120419bf832d71f04de6dfc1fdc69dd8
“Zhehao <2471437Z@student.gla.ac.uk>
2021-07-30 04:59:53 +0100

    Chapter6 exercise

--- rango/urls.py
+++ rango/urls.py
@@ -8,4 +8,5 @@ app_name = 'rango'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
+]
--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
+from rango.models import Page
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-    return render(request, 'rango/index.html', context=context_dict)
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+    return render(request, 'rango/index.html', context_dict)
 def about(request): 
     return HttpResponse("Rango says here is the about page <a href='/rango/'>index</a>") 
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+    return render(request, 'rango/category.html', context_dict)
