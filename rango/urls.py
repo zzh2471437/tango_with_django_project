96aff74f125c7917e2c6019074adea9520fd63ee
“Zhehao <2471437Z@student.gla.ac.uk>
2021-07-30 05:49:25 +0100

    Chapter7 ex

--- rango/urls.py
+++ rango/urls.py
@@ -6,7 +6,9 @@ from django.conf.urls.static import static
 app_name = 'rango'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
-    path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
+    url(r'^$', views.index, name='index'),
+    url(r'about/$', views.about, name='about'),
+    url(r'^add_category/$', views.add_category, name='add_category'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
+        views.show_category, name='show_category'),
 ]
